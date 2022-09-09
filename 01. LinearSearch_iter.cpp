0df4699cfa14ab936b091ba5662f41bc70a6db33
Hriskata <105062473+Hriskata@users.noreply.github.com>
2022-09-10 01:05:39 +0300

    Update 01. LinearSearch_iter.cpp

--- 01. LinearSearch_iter.cpp
+++ 01. LinearSearch_iter.cpp
@@ -7,9 +7,9 @@ int linearSearch(const T* arr, int len, T searched)
 	for (int i = 0; i < len; i++)
 	{
 		if (arr[i] == searched)
-			return i;//връщаме индекса.
+			return i;
 	}
-	return -1;//елементът го няма
+	return -1;
 }
 
 int main()
